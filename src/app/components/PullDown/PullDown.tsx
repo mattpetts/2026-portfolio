4605dfc4ac688eec86abd6e72f4a6b6fa07a1698
mattpetts <mattpetts23@gmail.com>
2026-02-10 08:44:42 +0000

    animate background

--- src/app/components/PullDown/PullDown.tsx
+++ src/app/components/PullDown/PullDown.tsx
@@ -9,13 +9,12 @@ const PullDown = ({ children }: { children: React.ReactNode }) => {
 
     return (
         <motion.div
-            className="flex-1 relative w-full z-20"
-            animate={{ y: isNavOpen ? 420 : 0 }}
-            initial={false}
+            className="flex-1 w-full z-20 h-screen overflow-y-scroll fixed"
+            animate={{ y: isNavOpen ? window.innerHeight / 2 : 0 }}
             transition={{ type: "spring", stiffness: 600, damping: 45 }}
             style={{
                 boxShadow: isNavOpen
-                    ? "0px 0px 70px 60px rgba(0,0,0,0.8)"
+                    ? "0px 50px 30px 60px rgba(0,0,0,0.8)"
                     : "0px 0px 0px rgba(0,0,0,0)",
             }}
         >
--- src/app/sections/Hero.tsx
+++ src/app/sections/Hero.tsx
@@ -5,8 +5,8 @@ import { Section } from "@/app/components";
 export const Hero = () => {
 
     return (
-        <Section classes={`bg-gray-100 text-black dark:bg-slate-900 dark:text-white flex items-center justify-center flex-col`}>
-            <h1 className="text-6xl font-black text-gradient-brand">Matthew Petts</h1>
+        <Section classes={`bg-gradient-to-br from-gray-50 to-gray-300 animate-gradient text-black dark:from-gray-700 dark:to-slate-900 dark:text-white flex items-center justify-center flex-col`}>
+            <h1 className="text-6xl font-black text-gradient-brand text-shadow-sm">Matthew Petts</h1>
             <span className="text-lg font-black">Front-end focused software engineer</span>
         </Section>
     );
