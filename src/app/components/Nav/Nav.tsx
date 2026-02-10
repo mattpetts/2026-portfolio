fff9f33c8ef86f5fa1d6b8234c6dc9b6434d2868
mattpetts <mattpetts23@gmail.com>
2026-02-10 08:53:49 +0000

    update brand gradients

--- src/app/components/Nav/Nav.tsx
+++ src/app/components/Nav/Nav.tsx
@@ -3,10 +3,10 @@
 const Nav = () => {
     return (
         <nav className={'h-6/12 p-8 overflow-auto w-full bg-slate-900 text-white dark:bg-white dark:text-black z-0 fixed flex flex-col items-center justify-center gap-8'}>
-            <a className="text-3xl font-black text-gradient-brand hover:underline cursor-pointer">Home</a>
-            <a className="text-3xl font-black text-gradient-brand hover:underline cursor-pointer">About me</a>
-            <a className="text-3xl font-black text-gradient-brand hover:underline cursor-pointer">Projects</a>
-            <a className="text-3xl font-black text-gradient-brand hover:underline cursor-pointer">Contact</a>
+            <a className="text-3xl font-black text-gradient-inverse hover:underline cursor-pointer">Home</a>
+            <a className="text-3xl font-black text-gradient-inverse hover:underline cursor-pointer">About me</a>
+            <a className="text-3xl font-black text-gradient-inverse hover:underline cursor-pointer">Projects</a>
+            <a className="text-3xl font-black text-gradient-inverse hover:underline cursor-pointer">Contact</a>
         </nav>
     );
 };
--- src/app/sections/Hero.tsx
+++ src/app/sections/Hero.tsx
@@ -6,8 +6,10 @@ export const Hero = () => {
 
     return (
         <Section classes={`bg-gradient-to-br from-gray-50 to-gray-300 animate-gradient text-black dark:from-gray-700 dark:to-slate-900 dark:text-white flex items-center justify-center flex-col`}>
-            <h1 className="text-6xl font-black text-gradient-brand text-shadow-sm">Matthew Petts</h1>
-            <span className="text-lg font-black">Front-end focused software engineer</span>
+            <div className="w-fit text-left">
+                <h1 className="text-6xl font-black text-shadow-sm">Matthew Petts</h1>
+                <span className="text-lg font-black text-gradient-brand">Front-end focused software engineer</span>
+            </div>
         </Section>
     );
 };
