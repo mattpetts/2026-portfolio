e3f9a6e8e7e61336b628cf54e31c2d421349c626
mattpetts <matt.petts@gmail.com>
2026-02-27 08:24:13 +0000

    projects animate on hover

--- src/app/components/Project/Project.tsx
+++ src/app/components/Project/Project.tsx
@@ -10,7 +10,7 @@ interface ProjectProps {
 
 const Project = ({ title, description, github, url, featured = false }: ProjectProps) => {
     return (
-        <div className={`border  dark:border-slate-700 p-8 flex flex-col gap-4 ${ featured ? 'shadow-md border-image-brand' : 'border-gray-400' }`}>
+        <div className={`border dark:border-slate-700 p-8 flex flex-col gap-4 ${ featured ? 'shadow-md border-image-brand' : 'border-gray-400' } hover:scale-101 transition-all duration-15o`}>
             <div className="flex justify-between items-center">
                 <h2 className={`text-lg sm:text-xl font-bold ${ featured ? 'text-gradient-brand dark:text-gradient-inverse' : 'text-slate-700 dark:text-white' }`}>{ title }</h2>
                 {featured && <span className="bg-gradient dark:bg-gradient-inverse text-white dark:text-slate-900 rounded px-2 py-1 text-xs font-black uppercase">In Progress</span>}
