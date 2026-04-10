9056d34e5fcc14efd826297074db9aafa01d14ea
mattpetts <matt.petts@gmail.com>
2026-04-10 19:33:10 +0100

    remove test that causes typescript error

--- __tests__/utils/cookies.test.ts
+++ __tests__/utils/cookies.test.ts
@@ -1,11 +1,6 @@
 import { setCookie } from "@/app/utils/cookies";
 
 describe('setCookie', () => {
-    test('return null if no cookie name is provided', () => {
-        expect(setCookie()).toBe(null);
-        expect(setCookie(false)).toBe(null);
-    });
-
     test('sets a cookie with the correct name and value', () => {
         setCookie('foo', 'bar');
         expect(document.cookie).toContain('foo=bar');
