0cadabe83b3773fba3b68e20ddcb000515b2bdbc
mattpetts <matt.petts@gmail.com>
2026-06-03 21:55:30 +0100

    add new project

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -7,39 +7,46 @@ export const PROJECTS = [
     },
     {
         id: 2,
+        title: "CRT UI",
+        description: "A retro terminal inspired React UI library",
+        github: "https://github.com/mattpetts/crt-ui",
+        featured: true,
+    },
+    {
+        id: 3,
         title: "Dictionary App",
         description: "A single-page dictionary interface built with React and Vite and using the dictionaryapi.dev REST api.",
         github: "https://github.com/mattpetts/dictionary-app",
         url: "https://dictionary-app-766.pages.dev/"
     },
     {
-        id: 3,
+        id: 4,
         title: "LightBoard",
         description: "A configurable Lightboard/grid app built with object-oriented vanilla JavaScript. Move your mouse over the grid to light up the tiles.",
         github: "https://github.com/mattpetts/lightboard"
     },
     {
-        id: 4,
+        id: 5,
         title: "Text Analyser",
         description: "A single page application for calculating character count, letter density, reading time and more! Built in React with Vite.",
         github: "https://github.com/mattpetts/text-analyser",
         url: "https://text-analyser.pages.dev/"
     },
     {
-        id: 5,
+        id: 6,
         title: "Rock, Paper, Scissors Node ",
         description: "A quick NodeJs build of Rock Paper Scissors that you can play in your IDE terminal.",
         github: "https://github.com/mattpetts/Rock-Paper-Scissors-Node"
     },
     {
-        id: 6,
+        id: 7,
         title: "Progress Tracker",
         description: "A small React/Vite project that tracks time remaining until key events",
         github: "https://github.com/mattpetts/progress-tracker",
         url: "https://progress-tracker-9oq.pages.dev/"
     },
     {
-        id: 7,
+        id: 8,
         title: "PokeAPI",
         description: "A lightweight API built with Hono and using Zod validation",
         github: "https://github.com/mattpetts/poke-api",
